0149bae42c6449677eac3bc5618064d092efb385
GervaisdeM <gervais@demontbrun.com>
2016-01-17 11:54:57 -0400

    Prepare for release of 2.0.1 | package party

--- package.php
+++ package.php
@@ -2,7 +2,7 @@
 
 require_once 'PEAR/PackageFileManager2.php';
 
-$version = '2.0.0';
+$version = '2.0.1';
 $notes = <<<EOT
 see ChangeLog
 EOT;
@@ -16,7 +16,7 @@ PEAR::setErrorHandling(PEAR_ERROR_DIE);
 
 $result = $package->setOptions(
 	array(
-		'filelistgenerator' => 'svn',
+		'filelistgenerator' => 'file',
 		'simpleoutput'      => true,
 		'baseinstalldir'    => '/',
 		'packagedirectory'  => './',
