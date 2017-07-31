7c4276617cae073156a47f8481bf6b4128b6956a
Michael Gauthier <mike@silverorange.com>
2017-07-31 11:50:31 -0300

    Use DateTime class in test

--- tests/test.php
+++ tests/test.php
@@ -7,10 +7,10 @@ $feed->title = 'My test feed';
 $feed->addNameSpace('g', 'http://base.google.com/ns/1.0');
 $feed->addAuthor(new AtomFeedAuthor('Nick Burka', 'nick@silverorange.com', 'http://www.silverorange.com'));
 $feed->link = new AtomFeedLink('http://www.silverorange.com', 'self');
-$feed->updated = new Date();
+$feed->updated = new DateTime();
 
 for ($i = 0; $i <= 10; $i++) {
-	$entry = new AtomFeedEntry('entry'.$i, 'Entry '.$i, new Date());
+	$entry = new AtomFeedEntry('entry'.$i, 'Entry '.$i, new DateTime());
 	$entry->contributor = new AtomFeedAuthor('Nick Burka', 'nick@silverorange.com', 'http://www.silverorange.com');
 	$entry->addAuthor(new AtomFeedAuthor('Nick Burka', 'nick@silverorange.com', 'http://www.silverorange.com'));
 	$feed->addEntry($entry);
