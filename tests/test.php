c527792a2ff5eab7c057d8f0f01417aaeed98257
Michael Gauthier <mike@silverorange.com>
2017-07-31 12:16:57 -0300

    CS fixes

--- tests/test.php
+++ tests/test.php
@@ -1,20 +1,36 @@
 <?php
 
-require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__.'/vendor/autoload.php';
 
 header("Content-type: text/xml");
 
 $feed = new AtomFeed();
 $feed->title = 'My test feed';
 $feed->addNameSpace('g', 'http://base.google.com/ns/1.0');
-$feed->addAuthor(new AtomFeedAuthor('Nick Burka', 'nick@silverorange.com', 'http://www.silverorange.com'));
+$feed->addAuthor(
+	new AtomFeedAuthor(
+		'Nick Burka',
+		'nick@silverorange.com',
+		'http://www.silverorange.com'
+	)
+);
 $feed->link = new AtomFeedLink('http://www.silverorange.com', 'self');
 $feed->updated = new DateTime();
 
 for ($i = 0; $i <= 10; $i++) {
 	$entry = new AtomFeedEntry('entry'.$i, 'Entry '.$i, new DateTime());
-	$entry->contributor = new AtomFeedAuthor('Nick Burka', 'nick@silverorange.com', 'http://www.silverorange.com');
-	$entry->addAuthor(new AtomFeedAuthor('Nick Burka', 'nick@silverorange.com', 'http://www.silverorange.com'));
+	$entry->contributor = new AtomFeedAuthor(
+		'Nick Burka',
+		'nick@silverorange.com',
+		'http://www.silverorange.com'
+	);
+	$entry->addAuthor(
+		new AtomFeedAuthor(
+			'Nick Burka',
+			'nick@silverorange.com',
+			'http://www.silverorange.com'
+		)
+	);
 	$feed->addEntry($entry);
 }
 
